0a4d58873bea4c4258b85693a2f0cdd52b810ff0
Hersh Rudrawal <45576093+HershR@users.noreply.github.com>
2024-05-04 15:23:50 -0700

    Update PlayerStatsSO
    Moved it to separate file

--- Assets/Scripts/Stats/StatSO.cs
+++ Assets/Scripts/Stats/StatSO.cs
@@ -1,6 +1,3 @@
-using AYellowpaper.SerializedCollections;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 
@@ -12,21 +9,24 @@ public class StatSO : ScriptableObject
     [field: SerializeField] public int MaxLevel { get; private set; } = 5;
     [field: SerializeField] public Sprite Icon { get; private set; }
 
-    [SerializeField] [TextArea] [Tooltip("Add to array if Description of Stat changes with each upgrade. Main use for Research Upgrades")]
+    [SerializeField]
+    [TextArea]
+    [Tooltip("Add to array if Description of Stat changes with each upgrade. Main use for Research Upgrades")]
     private string[] description = new string[1];
-    
-    [SerializeField] [Tooltip("Cost to upgrade Stat at each level under MaxLevel")]
+
+    [SerializeField]
+    [Tooltip("Cost to upgrade Stat at each level under MaxLevel")]
     private int[] levelCosts = new int[4];
 
     public void Upgrade()
     {
-        if(Level == MaxLevel) { return; }
+        if (Level == MaxLevel) { return; }
         Level += 1;
     }
     public int GetCost()
     {
-        if(Level >= MaxLevel) { return -1; }
-        return levelCosts[Level];
+        if (Level >= MaxLevel) { return -1; }
+        return levelCosts[Mathf.Min(levelCosts.Length, Level)];
     }
 
     public string GetDescription()
@@ -35,31 +35,6 @@ public class StatSO : ScriptableObject
     }
 }
 
-[CreateAssetMenu(fileName = "new PlayerStats", menuName = "ScriptableObjects/PlayerStats")]
-public class PlayerStatsSO : ScriptableObject
-{
-    /*
-     Holds the Players Total Money
-     and Holds the Players Stat Levels in a Dict<StatType, StatSO>
-     */
-    [field: SerializeField] public int Tokens { get; private set; } = 0;
-    [field: SerializeField] public SerializedDictionary<StatType, StatSO> Stats { get; private set; }
 
-    public bool IsUpgradable(StatType type)
-    {
-        var stat = Stats[type];
-        if (stat.Level == stat.MaxLevel) { return false; }
-        if (stat.GetCost() > Tokens) { return false; }
-        return true;
-    }
-
-    public void UpgradeStat(StatType type)
-    {
-        if (!IsUpgradable(type)) { return; }
-        Stats[type].Upgrade();
-
-    }
-
-}
 //Differnt type of upgradeable stats
-public enum StatType { Health, Speed, Dash, DashRefillParry, Food1Spawn, Food2S, DashRefillSpawn, TokenSpawn, DroneCollectionRate, DroneSpawn, OilResearch, TrashResearch, AcidityResearch };
+public enum StatType { Health, Speed, Dash, DashRefillParry, Food1Spawn, Food2Spawn, DashRefillSpawn, TokenSpawn, DroneCollectionRate, DroneSpawn, OilResearch, TrashResearch, AcidityResearch };
