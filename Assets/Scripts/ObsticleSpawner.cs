a103209654b5b439e593b8c2ce61bef480e9edcc
G-Pasko <gpasko@ucsc.edu>
2024-05-31 19:20:03 -0700

    done

--- Assets/Scripts/ObsticleSpawner.cs
+++ Assets/Scripts/ObsticleSpawner.cs
@@ -17,37 +17,71 @@ public enum InteractableType
 
 public class ObsticleSpawner : MonoBehaviour
 {
-	[SerializedDictionary("Interactable", "Weight")]
-	[SerializeField] public SerializedDictionary<InteractableType, int> InteractableWeights;
+	// Initialize Enviornment Generator var
+	[SerializeField] public EnvironmentGenerator environmentGenerator;
+
+	[SerializeField] public SerializedDictionary<InteractableType, int> InteractableWeights; // Serves no purpose anymore
+
+	// Public dictionary mapping obsticle types types to game objects
 	[SerializeField] public SerializedDictionary<InteractableType, GameObject> InteractableObjects;
 
-	
-	
+	// Initialize current env var
+	[SerializeField] public EnvironmentType CurrentEnviornment;
 
-	// We get env passed in
-	// From that enum, we get 
+	// Initialize spawn weight dictionaries for ALL enviornments
+	[SerializeField] public SerializedDictionary<InteractableType, int> NormalEnv;
+	[SerializeField] public SerializedDictionary<InteractableType, int> OilEnv;
+	[SerializeField] public SerializedDictionary<InteractableType, int> TrashEnv;
+	[SerializeField] public SerializedDictionary<InteractableType, int> CoralEnv;
 
-	float timer = 0f;
+	// Create dictionary mapping enviornments to their respective spawn dicts
+	[SerializeField]
+	private SerializedDictionary<EnvironmentType, SerializedDictionary<InteractableType, int>> EnvWeightDict =
+			new SerializedDictionary<EnvironmentType, SerializedDictionary<InteractableType, int>> { };
+
+	// Initialize spawn rate upgrades from player
+	[SerializeField] public StatSO tokenSpawnUpgrade;
+	[SerializeField] public StatSO kelpSpawnUpgrade;
+	[SerializeField] public StatSO jellyfishSpawnUpgrade;
+	[SerializeField] public StatSO dashSpawnUpgrade;
+	[SerializeField] public StatSO oilSpawnUpgrade;
+	[SerializeField] public StatSO trashSpawnUpgrade;
+	[SerializeField] public StatSO acidityUpgrade;
+
+	float timer = 0f; // Spawn rate timer
 	private int totalWeight = 0;
-	private List<(InteractableType, int)> objWeights = new List<(InteractableType, int)>{};
+	// List of obsticles and their spawn weights (affected by player upgrades)
+	private List<(InteractableType, int)> objWeights = new List<(InteractableType, int)>();
 
 	private void Start()
 	{
-		foreach((InteractableType t, int i) in InteractableWeights)
-        {
-			if(i > 0)
-            {
-				objWeights.Add((t, i + totalWeight));
-				totalWeight += i;
-			}
-        }
-		Debug.Log(InteractableWeights);
+		// Get Current env
+		CurrentEnviornment = environmentGenerator.CurrentEnvironmentToSpawn;
+		// Populate Weight dictionaries based on current enviornemnt
+		EnvWeightDict.Add(EnvironmentType.Normal, NormalEnv);
+		EnvWeightDict.Add(EnvironmentType.OilField, OilEnv);
+		EnvWeightDict.Add(EnvironmentType.CoralReef, CoralEnv);
+		EnvWeightDict.Add(EnvironmentType.TrashField, TrashEnv);
+		UpdateObsticleWeights();
+
+		// Fill dict with env values (make ur own dict)
+		UpdateObsticleWeights();
 		Debug.Log("Total weight:" + totalWeight);
 		Debug.Log("(Objects, weights): " + objWeights);
+		for (int i = 0; i < objWeights.Count; i++)
+		{
+			Debug.Log(i + ": " + objWeights[i]);
+		}
 	}
 
 	void Update()
 	{
+		if (CurrentEnviornment != environmentGenerator.CurrentEnvironmentToSpawn && environmentGenerator.CurrentEnvironmentToSpawn != EnvironmentType.Transition)
+		{
+			CurrentEnviornment = environmentGenerator.CurrentEnvironmentToSpawn;
+			Debug.Log("New enviornemnt: " + CurrentEnviornment);
+			UpdateObsticleWeights();
+		}
 		if (timer <= 3f)
 		{
 			timer += Time.deltaTime;
@@ -55,18 +89,58 @@ public class ObsticleSpawner : MonoBehaviour
 		else
 		{
 			timer = 0;
-			int index = Random.Range(0, totalWeight);
-			foreach ((InteractableType t, int i) in objWeights)
+			for (int i = 0; i < Random.Range(1, 4); i++)
 			{
-				if (i > index)
+				int index = Random.Range(0, totalWeight);
+				foreach ((InteractableType t, int w) in objWeights)
 				{
-					GameObject new_obsticle = Instantiate(InteractableObjects[t], this.transform);
-					new_obsticle.GetComponent<ObsticleController>().obsticle_type = t;
-					Debug.Log("Spawned a " + t);
-					break;
+					if (w > index)
+					{
+						Debug.Log("About to spawn a: " + InteractableObjects[t] + " at " + this.transform.position);
+						GameObject new_obsticle = Instantiate(InteractableObjects[t], this.transform);
+						new_obsticle.GetComponent<ObsticleController>().obsticle_type = t;
+						Debug.Log("Spawned a " + t);
+						break;
+					}
 				}
+				Debug.Log("obsticle spawned");
 			}
-			Debug.Log("obsticle spawned");
+
 		}
 	}
+	// grab values from corresponding dict
+	void UpdateObsticleWeights()
+	{
+		foreach ((InteractableType t, int i) in EnvWeightDict[CurrentEnviornment])
+		{
+			if (i > 0)
+			{
+				int weight = i;
+				switch (t)
+				{
+					case InteractableType.Tokens:
+						weight *= 1 + tokenSpawnUpgrade.Level;
+						break;
+					case InteractableType.JellyFish:
+						weight *= 1 + jellyfishSpawnUpgrade.Level * acidityUpgrade.Level;
+						break;
+					case InteractableType.Kelp:
+						weight *= 1 + kelpSpawnUpgrade.Level * acidityUpgrade.Level;
+						break;
+					case InteractableType.Trash:
+						weight /= 1 + trashSpawnUpgrade.Level;
+						break;
+					case InteractableType.Oil:
+						weight /= 1 + oilSpawnUpgrade.Level;
+						break;
+				}
+				objWeights.Add((t, weight + totalWeight));
+				totalWeight += weight;
+			}
+		}
+		Debug.Log("New Obsticle Spawn Rates: " + string.Join(", ", objWeights));
+	}
+	// THIS IS WHERE ALL THE ENV DICTIONARIES WILL GO IF WE MAKE EM PRIVATE!!!!
+
+
 }
